bf790789fef3a914bbc04b3f6a9d18a89fdd978a
xinghuo1990 <xinghuo1990@163.com>
2019-08-29 10:25:12 +0800

    新增注释

--- src/main/java/com/SpringbootApplication.java
+++ src/main/java/com/SpringbootApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Description
+ * <br> 启动类
+ * @date 2019-08-29
+ * @author lichengqiang
+ **/
 @SpringBootApplication
 public class SpringbootApplication {
 
